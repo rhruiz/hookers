2f44b701872452d2d4888a259cb56387e56c37fb
Flavio Andrade <flavio.andrade@locaweb.com.br>
2012-05-10 16:40:52 -0300

    "teste"

--- lib/hookers/git/repository.rb
+++ lib/hookers/git/repository.rb
@@ -1,10 +1,14 @@
 module Hookers
-  module Git
-    class Repository
-      def last_commit
-        revision = %x(git log -1 HEAD --format=%H%n%an%n%s).split("\n")
-        Git::Commit.new(revision[0], revision[1], revision[2])
-      end
-    end
-  end
- end
+	module Git
+		class Repository
+			def last_commit
+				revision = %x(git log -1 HEAD --format=%H%n%an%n%s).split("\n")
+				Git::Commit.new(revision[0], revision[1], revision[2])
+			end
+
+			def commit(message)
+				%x(git commit -m'"#{message}"')
+			end
+		end
+	end
+end
